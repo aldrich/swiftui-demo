0bf229634c68722832c2fab6ebbb93cc5fe536ea
Aldrich Co <aco@freelancer.com>
2020-07-15 20:47:53 +0800

    Initial checkin

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Aldrich Co on 7/15/20.
+//  Copyright © 2020 Aldrich Co. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
