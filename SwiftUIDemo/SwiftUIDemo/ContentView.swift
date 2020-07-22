19b9fe35638fa4b8a9972c71b3c48f4ea8a909e8
Aldrich Co <aco@freelancer.com>
2020-07-22 20:53:09 +0800

    Basic look and feel

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -19,6 +19,42 @@ struct Event: Identifiable {
 
 struct ContentView: View {
 	
+	var events: [Event] = [
+		Event(date: "Today",
+			  name: "Mexico Home Show Autumn 2020",
+			  location: "Queen Alexandria AOC Covfefe Center",
+			  isFree: true,
+			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
+		
+		Event(date: "Today",
+			  name: "Mecixo Home Show Autumn 2020",
+			  location: "Queen Alexandria AOC Covfefe Center",
+			  isFree: true,
+			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
+		
+		Event(date: "Today",
+			  name: "Mecixo Home Show Autumn 2020",
+			  location: "Queen Alexandria AOC Covfefe Center",
+			  isFree: true,
+			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
+	]
+	
+	var body: some View {
+		
+		GeometryReader { geometry in
+			ScrollView {
+				VStack(alignment: .leading) {
+					CityViewExpanded(width: geometry.size.width)
+					EventsList(events: self.events)
+						.padding(.bottom, 20)
+				}
+			}
+		}.edgesIgnoringSafeArea(.all)
+	}
+}
+
+struct CityViewExpanded: View {
+	
 	let imageName = "filip-gielda-QKrLdkoYDrc-unsplash"
 	
 	let heading = "MEXICO CITY"
@@ -37,165 +73,140 @@ struct ContentView: View {
 		"davis-vargas-2vSNlKHn9h0-unsplash",
 	]
 	
-	var events: [Event] = [
-		Event(date: "Today",
-			  name: "Mecixo Home Show Autumn 2020",
-			  location: "Queen Alexandria AOC Covfefe Center",
-			  isFree: true,
-			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
-		
-		Event(date: "Today",
-			  name: "Mecixo Home Show Autumn 2020",
-			  location: "Queen Alexandria AOC Covfefe Center",
-			  isFree: true,
-			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
-		
-		Event(date: "Today",
-			  name: "Mecixo Home Show Autumn 2020",
-			  location: "Queen Alexandria AOC Covfefe Center",
-			  isFree: true,
-			  thumbImage: "davis-vargas-2vSNlKHn9h0-unsplash"),
-	]
+	let width: CGFloat
+	
+	init(width: CGFloat) {
+		self.width = width
+	}
 	
 	var body: some View {
-		NavigationView {
+		VStack {
+			Image(self.imageName)
+				.resizable()
+				.frame(width: self.width,
+					   height: 240, alignment: .center)
+				.scaledToFit()
+				.padding(.bottom, 20)
 			
-			GeometryReader { geometry in
+			HStack {
 				
-				ScrollView {
+				VStack(alignment: .leading, spacing: 2) {
+					Text(self.heading)
+						.font(.system(size: 24, weight: .black))
 					
-					VStack(alignment: .leading, spacing: 4) {
-						
-						Image(self.imageName)
+					Text(self.subText)
+						.font(.system(size: 16, weight: .regular))
+					
+				}
+				.padding(.horizontal, 20)
+				
+				Spacer()
+				
+				VStack(alignment: .trailing, spacing: 2) {
+					Text("18ºC")
+						.font(.system(size: 24, weight: .black))
+					
+					Text("Cloudy")
+						.font(.system(size: 16, weight: .regular))
+				}
+				.foregroundColor(.gray)
+				.padding(.horizontal, 20)
+			}
+			.padding(.bottom, 20)
+			
+			// this is the divider line to the next section
+			Color(white: 0.4)
+				.frame(width: (width - 20) / 2, height: 1)
+				.padding(.leading, 0)
+				.padding(.bottom, 24)
+			
+			// --- (Carousel) TOUR PACKAGES
+			
+			// ----- Header
+			HStack {
+				Text(self.carouselTitle)
+					.font(.system(size: 18, weight: .medium))
+					.padding(.leading, 20)
+				Spacer()
+				
+				Text(self.carouselMoreTitle)
+					.padding(.trailing, 20)
+			}
+			
+			ScrollView(.horizontal) {
+				HStack {
+					ForEach(0..<self.carouselImages.count) {
+						Image(self.carouselImages[$0])
 							.resizable()
-							.frame(width: geometry.size.width, height: 240, alignment: .center)
-							.scaledToFit()
-							.padding(.bottom, 20)
+							.aspectRatio(contentMode: .fill)
+							.frame(width: self.width * 0.65,
+								   height: 140)
+							.cornerRadius(4)
+							.padding(.horizontal, 1)
 						
-						HStack {
-							
-							VStack(alignment: .leading, spacing: 2) {
-								Text(self.heading)
-									.font(.system(size: 24, weight: .black))
-								
-								Text(self.subText)
-									.font(.system(size: 16, weight: .regular))
-								
-							}
-							.padding(.horizontal, 20)
-							
-							Spacer()
-							
-							VStack(alignment: .trailing, spacing: 2) {
-								Text("18ºC")
-									.font(.system(size: 24, weight: .black))
-								
-								Text("Cloudy")
-									.font(.system(size: 16, weight: .regular))
-							}
-							.foregroundColor(.gray)
-							.padding(.horizontal, 20)
-						}
-						.padding(.bottom, 20)
-						
-						// this is the divider line to the next section
-						Color(white: 0.2)
-							.frame(width: 100, height: 1)
-							.padding(.leading, 20)
-							.padding(.bottom, 24)
-						
-						// --- (Carousel) TOUR PACKAGES
-						
-						// ----- Header
-						HStack {
-							Text(self.carouselTitle)
-								.font(.system(size: 18, weight: .medium))
-								.padding(.leading, 20)
-							Spacer()
-							
-							Text(self.carouselMoreTitle)
-								.padding(.trailing, 20)
-						}
-						
-						ScrollView(.horizontal) {
-							HStack {
-								ForEach(0..<self.carouselImages.count) {
-									Image(self.carouselImages[$0])
-										.resizable()
-										.aspectRatio(contentMode: .fill)
-										.frame(width: geometry.size.width * 0.65, height: 140)
-										.cornerRadius(4)
-										.padding(.horizontal, 1)
-									
-								}
-							}
-							.padding(.vertical, 16)
-							.padding(.horizontal, 20)
-						}
-						EventsList(events: self.events)
 					}
 				}
-				.edgesIgnoringSafeArea(.all)
+				.padding(.vertical, 16)
+				.padding(.horizontal, 20)
 			}
-			.navigationBarTitle("Hey")
 		}
-    }
+	}
 }
 
 struct EventsList: View {
 	
 	// Events List
 	let eventsHeaderTitle = "Mexico City Events"
-	let eventsSubHeaderTitle = "Relevant"
+	let eventsSubHeaderTitle = "Relevance"
 	
 	var events: [Event]
 	
 	var body: some View {
 		
-		ScrollView(showsIndicators: false) {
+		VStack {
+			// (List) CITY EVENTS
+			// ----- Header
+			HStack {
+				Text(self.eventsHeaderTitle)
+					.font(.custom("OpenSans-Bold", size: 24))
+					.padding(.leading, 20)
+				
+				Spacer()
+				
+				Text(self.eventsSubHeaderTitle)
+					.font(.custom("OpenSans-Regular", size: 14))
+					.foregroundColor(.init(white: 0.3))
+					.padding(.trailing, 20)
+			}
 			
 			
-			VStack {
-				// (List) CITY EVENTS
-				// ----- Header
+			ForEach(events) { (event: Event) in
 				HStack {
-					Text(self.eventsHeaderTitle)
-						.font(.system(size: 24, weight: .black))
+					
+					Image(event.thumbImage)
+						.resizable()
+						.aspectRatio(contentMode: .fill)
+						.frame(width: 96, height: 96, alignment: .center)
+						.clipped()
+						.cornerRadius(4)
 						.padding(.leading, 20)
 					
-					Spacer()
 					
-					Text(self.eventsSubHeaderTitle)
-						.padding(.trailing, 20)
-				}
-				
-				
-				ForEach(events) { (event: Event) in
-					HStack {
-						Image(event.thumbImage)
-							.resizable()
-							.frame(width: 96, height: 96)
-							.scaledToFit()
-							.cornerRadius(4)
-							.padding(.leading, 20)
+					VStack(alignment: .leading) {
+						Text(event.date)
+							.font(.system(size: 12, weight: .regular))
+							.padding(.bottom, 6)
 						
+						Text(event.name)
+							.font(.system(size: 15, weight: .bold))
+							.padding(.bottom, 12)
 						
-						VStack(alignment: .leading) {
-							Text(event.date)
-								.font(.system(size: 12, weight: .regular))
-								.padding(.bottom, 6)
-							
-							Text(event.name)
-								.font(.system(size: 15, weight: .bold))
-								.padding(.bottom, 12)
-							
-							Text(event.location)
-								.font(.system(size: 12, weight: .regular))
-						}
-						.padding(.trailing, 20)
-						
-						Spacer()
+						Text(event.location)
+							.font(.system(size: 12, weight: .regular))
 					}
+					.padding(.trailing, 20)
+					
+					Spacer()
 				}
 			}
 		}
@@ -216,7 +227,7 @@ struct ContentView_Previews: PreviewProvider {
 			sampleEvent
 		}
 	}()
-
+	
 	
 	static var previews: some View {
 		ContentView()
